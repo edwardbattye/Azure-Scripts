552d3182d618740f6ae5ca1706aa591d4b3bde80
edbat <edwardbattye@gmail.com>
2023-02-24 10:49:53 +0000

    Create azure_join_laptop.ps1

---
+++ azure_join_laptop.ps1
@@ -0,0 +1,14 @@
+﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Import-Module PowerShellGet
+New-Item -Path 'C:\Program Files'-Name 'WindowsPowershellScripts' -ItemType "directory" -Force
+$Path = 'C:\Program Files'
+$repo = @{
+    Name = 'MyRepository'
+    SourceLocation = $Path
+    PublishLocation = $Path
+    InstallationPolicy = 'Trusted'
+}
+Register-PSRepository @repo
+
+Install-Script -Name Get-WindowsAutoPilotInfo
+Get-WindowsAutoPilotInfo.ps1 -Online -AddToGroup "Group_Name" -Assign
